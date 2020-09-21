54a754779064ae302e69bf6e3b637125e4978522
DerianRS <derian.rods@gmail.com>
2020-09-21 06:56:35 +0200

    🔧  docs

---
+++ docs/precache-manifest.b93f1acc06220e7561c82ca1eab7b69e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15f6227e85991a3b6d055a71fdc1323e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "217fcf57e0a5f05682d2",
+    "url": "/static/css/main.f02635a3.chunk.css"
+  },
+  {
+    "revision": "8499c1bd605f3dea86ca",
+    "url": "/static/js/2.1ac15f8f.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.1ac15f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217fcf57e0a5f05682d2",
+    "url": "/static/js/main.276eeb45.chunk.js"
+  },
+  {
+    "revision": "503902cd95081a820b5d",
+    "url": "/static/js/runtime-main.d105fe37.js"
+  }
+]);
